65ef2b5333046c10cc9e147e87eeff77a4279c29
John Deegan <john.deegan@hotmail.com>
2017-06-14 01:20:38 +0100

    initial commit

---
+++ src/components/Container.js
@@ -0,0 +1,19 @@
+//dependencies and styles
+import React, {Component} from 'react';
+import * as RB from 'react-bootstrap';
+import axios from 'axios';
+
+class Container extends Component{
+
+    constructor(props){
+        super();
+    }
+
+    render(){
+        return(
+            <h1>Hello World</h1>
+        )
+    }
+}
+
+export default Container;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Container from './components/Container';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<Container />, document.getElementById('root'));
+registerServiceWorker();
